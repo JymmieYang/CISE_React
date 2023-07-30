aee9dac9b22176d9ad69d072c84ced0daad345de
wsking233 <wsking233@gmail.com>
2023-07-30 16:53:24 +1200

    Update App.js

--- cise-react-learn/src/App.js
+++ cise-react-learn/src/App.js
@@ -16,6 +16,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Agile is about Values and Principles
+          wahahahha test
         </a>
       </header>
     </div>
